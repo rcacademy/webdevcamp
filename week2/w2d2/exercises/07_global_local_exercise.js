c37fdfbbcf084db2f5e4c72db4fed8555b67b994
Colin Loretz <colinloretz@gmail.com>
2016-04-13 20:12:53 -0700

    Update 07_global_local_exercise.js

--- week2/w2d2/exercises/07_global_local_exercise.js
+++ week2/w2d2/exercises/07_global_local_exercise.js
@@ -3,12 +3,11 @@
   Goal: To observe how local and global variables work with function scope.
   1. Create a function called displayBank()
   2. Create  global variable called bankBalance and set it to 10000.
-  3. Inside of displayBank, the first line should try to console.log the bankBalance global variable.
-  4. Next, create a new local variable inside of displayBank also called bankBalance and set it equal to 1000000.
-  5. Use console.log to display the bankBalance again.
-  6. Now call the displayBank functions
-  7. Finally, use console.log outside of the displayBank function to display the global bankBalance.
-  8. You should have two console.logs with numbers and one that is undefined - try to reason why this might be.
+  3. Inside of displayBank, create a new local variable inside of displayBank also called bankBalance and set it equal to 1000000.
+  4. Use console.log to display the bankBalance again.
+  5. Now call the displayBank functions
+  6. Finally, use console.log outside of the displayBank function to display the global bankBalance.
+  7. You should have three console.logs with numbers.
 
 */
 
