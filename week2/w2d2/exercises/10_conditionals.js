4f116892addd7b4c451ec587a698a33230a55077
Colin Loretz <colinloretz@gmail.com>
2016-04-13 20:01:09 -0700

    Update 10_conditionals.js

--- week2/w2d2/exercises/10_conditionals.js
+++ week2/w2d2/exercises/10_conditionals.js
@@ -5,7 +5,7 @@
   3. Make the function returnf alse if the number is not equal to zero.
 
   isString
-  1. Create a function called isZero that takes 1 argument called varToCheck
+  1. Create a function called isString that takes 1 argument called varToCheck
   2. Make the function return true if the argument is a string
   3. Make the function return false if the argument is not a string
 
